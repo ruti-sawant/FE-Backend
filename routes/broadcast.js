8ff59b9b9dbee969d6125cb6c2c6de94634cfcb6
Rutikesh Sawant <68364793+ruti-sawant@users.noreply.github.com>
2021-12-10 00:23:14 +0530

    Added routed to post broadcast add question and answer and to delete broadcast

--- routes/broadcast.js
+++ routes/broadcast.js
@@ -57,4 +57,85 @@ router.get("/farmer/:farmerId", (req, res) => {
         });
 });
 
+router.post("/", (req, res) => {
+    const data = req.body;
+    axios.post(process.env.API_URL + "/broadcasts", {
+        data
+    }, {
+        headers: {
+            'Content-Type': 'application/json',
+            'apiid': process.env.API_KEY
+        }
+    })
+        .then((data) => {
+            console.log("broadcast inserted", data);
+            res.status(200).send({ message: "Broadcast inserted successfully" });
+        })
+        .catch((err) => {
+            console.log("Broadcast insert err", data);
+            res.status(400).send({ message: err.message });
+        });
+});
+
+router.post("/insertQuestion/:broadcastId", (req, res) => {
+    const data = req.body;
+    const broadcastId = req.params.broadcastId;
+    axios.patch(process.env.API_URL + "/broadcasts/" + broadcastId, {
+        data
+    }, {
+        headers: {
+            'Content-Type': 'application/json',
+            'apiid': process.env.API_KEY
+        }
+    })
+        .then((data) => {
+            console.log("broadcast question inserted", data);
+            res.status(200).send({ message: "Broadcast question inserted successfully" });
+        })
+        .catch((err) => {
+            console.log("Broadcast question insert err", data);
+            res.status(400).send({ message: err.message });
+        });
+});
+
+router.post("/insertAnswer/:broadcastId/:chatId", (req, res) => {
+    const data = req.body;
+    const broadcastId = req.params.broadcastId;
+    const chatId = req.params.chatId;
+    axios.patch(process.env.API_URL + "/broadcasts/" + broadcastId + "/" + chatId, {
+        data
+    }, {
+        headers: {
+            'Content-Type': 'application/json',
+            'apiid': process.env.API_KEY
+        }
+    })
+        .then((data) => {
+            console.log("broadcast answer inserted", data);
+            res.status(200).send({ message: "Broadcast answer inserted successfully" });
+        })
+        .catch((err) => {
+            console.log("Broadcast answer insert err", data);
+            res.status(400).send({ message: err.message });
+        });
+});
+
+router.post("/delete/:broadcastId", (req, res) => {
+    const broadcastId = req.params.broadcastId;
+    axios.delete(process.env.API_URL + "/broadcasts/" + broadcastId, {
+        headers: {
+            'Content-Type': 'application/json',
+            'apiid': process.env.API_KEY
+        }
+    })
+        .then((data) => {
+            console.log("broadcast delete", data);
+            res.status(200).send({ message: "Broadcast deleted successfully" });
+        })
+        .catch((err) => {
+            console.log("Broadcast delete err", data);
+            res.status(400).send({ message: err.message });
+        });
+})
+
 export default router;
