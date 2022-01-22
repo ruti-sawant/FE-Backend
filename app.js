43689f9cbc0499889b6b3832553c109059fdf398
Harshal-9 <harshal.kodgire@walchandsangli.ac.in>
2022-01-22 15:27:33 +0530

    Added routes for CropMonitoring

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
 import express from "express";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 import fileUpload from "express-fileupload";
-import cors from 'cors';
+import cors from "cors";
 import axios from "axios";
 const app = express();
 
-
 //to allow get from anywhere.
 const corsOptions = {
-    origin: '*',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200
-}
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 app.use(fileUpload());
 app.use(express.json());
 
-
-
 //imports for routes.
-import uploadFile from './routes/uploadFile.js';
-import farmers from './routes/farmers.js';
-import dailyDiary from './routes/dailyDiary.js';
-import bulkUpload from './routes/bulkUpload.js';
-import seasonalData from './routes/seasonalData.js';
-import broadcasts from './routes/broadcast.js';
-import mrlReports from './routes/mrl.js';
+import uploadFile from "./routes/uploadFile.js";
+import farmers from "./routes/farmers.js";
+import dailyDiary from "./routes/dailyDiary.js";
+import bulkUpload from "./routes/bulkUpload.js";
+import seasonalData from "./routes/seasonalData.js";
+import broadcasts from "./routes/broadcast.js";
+import mrlReports from "./routes/mrl.js";
+import cropMonitoring from "./routes/cropMonitoring.js";
 
 app.use("/uploadFile", uploadFile);
 app.use("/farmers", farmers);
@@ -35,25 +33,27 @@ app.use("/bulkUpload", bulkUpload);
 app.use("/seasonalData", seasonalData);
 app.use("/broadcasts", broadcasts);
 app.use("/mrlReports", mrlReports);
+app.use("/cropMonitoring", cropMonitoring);
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log("Server started on port ", port);
+  console.log("Server started on port ", port);
 });
 
 //route to fetch filters from database.
 app.get("/filters", (req, res) => {
-    axios.get(process.env.API_URL + "/filters", {
-        headers: {
-            'Content-Type': 'application/json',
-            'apiid': process.env.API_KEY
-        }
+  axios
+    .get(process.env.API_URL + "/filters", {
+      headers: {
+        "Content-Type": "application/json",
+        apiid: process.env.API_KEY,
+      },
     })
-        .then((data) => {
-            res.status(200).send(data.data);
-        })
-        .catch((err) => {
-            res.status(400).send({ message: err.message });
-        });
+    .then((data) => {
+      res.status(200).send(data.data);
+    })
+    .catch((err) => {
+      res.status(400).send({ message: err.message });
+    });
 });
---
+++ routes/cropMonitoring.js
@@ -0,0 +1,111 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import axios from "axios";
+const router = express.Router();
+
+export default router;
+
+// router.get("/", (req, res) => {
+//   console.log(req.ip);
+//   res.send("Hi");
+// });
+
+// /cropMonitoring
+router.get("/", (req, res) => {
+  axios
+    .get(process.env.API_URL + "/cropMonitoring", {
+      headers: {
+        "Content-Type": "application/json",
+        apiid: process.env.API_KEY,
+      },
+    })
+    .then((data) => {
+      console.log("data", data.data);
+      res.status(200).send(data.data);
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.status(400).send({ message: err.message });
+    });
+});
+
+// /cropMonitoring/data/<monitoringId>
+router.get("/data/:monitoringId", (req, res) => {
+  const monitoringId = req.params.monitoringId;
+  axios
+    .get(process.env.API_URL + "/cropMonitoring/data/" + monitoringId, {
+      headers: {
+        "Content-Type": "application/json",
+        apiid: process.env.API_KEY,
+      },
+    })
+    .then((data) => {
+      console.log("data", data.data);
+      res.status(200).send(data.data);
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.status(400).send({ message: err.message });
+    });
+});
+
+// /cropMonitoring/MHCode/<MHCode>
+router.get("/MHCode/:MHCode", (req, res) => {
+  const MHCode = req.params.MHCode;
+  axios
+    .get(process.env.API_URL + "/cropMonitoring/MHCode/" + MHCode, {
+      headers: {
+        "Content-Type": "application/json",
+        apiid: process.env.API_KEY,
+      },
+    })
+    .then((data) => {
+      console.log("data", data.data);
+      res.status(200).send(data.data);
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.status(400).send({ message: err.message });
+    });
+});
+
+// delete according MonitoringId
+router.post("/delete/:monitoringId", (req, res) => {
+  const monitoringId = req.params.monitoringId;
+  axios
+    .delete(process.env.API_URL + "/cropMonitoring/" + monitoringId, {
+      headers: {
+        "Content-Type": "application/json",
+        apiid: process.env.API_KEY,
+      },
+    })
+    .then((data) => {
+      console.log("Crop Monitoring item deleted based on MonitoringId", data);
+      res.status(200).send({ message: "Data deleted successfully" });
+    })
+    .catch((err) => {
+      console.log("Deleting error", err);
+      res.status(400).send({ message: err.message });
+    });
+});
+
+// delete according MHCode
+router.post("/delete/MHCode/:MHCode", (req, res) => {
+  const MHCode = req.params.MHCode;
+  axios
+    .delete(process.env.API_URL + "/cropMonitoring/MHCode/" + MHCode, {
+      headers: {
+        "Content-Type": "application/json",
+        apiid: process.env.API_KEY,
+      },
+    })
+    .then((data) => {
+      console.log("Crop Monitoring item deleted based on MHCode", data);
+      res.status(200).send({ message: "Data deleted successfully" });
+    })
+    .catch((err) => {
+      console.log("Deleting error", err);
+      res.status(400).send({ message: err.message });
+    });
+});
