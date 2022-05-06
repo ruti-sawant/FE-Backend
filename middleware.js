261b08e0a98fb6b26db3a5ee7a1c50e6bd8e25e0
Rutikesh Sawant <68364793+ruti-sawant@users.noreply.github.com>
2022-05-06 15:38:39 +0530

    Changed allow headers in middleware

--- middleware.js
+++ middleware.js
@@ -6,7 +6,8 @@ function middleware(req, res, next) {
     if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
         console.log("correct origin");
         res.header('Access-Control-Allow-Origin', req.headers.origin);
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        res.header('Access-Control-Allow-Headers', '*');
         res.header('Access-Control-Allow-Credentials', true);
         res.header("Access-Control-Allow-Methods", "*");
     }
