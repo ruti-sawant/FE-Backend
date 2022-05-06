12bcd686048396c2305a4f502dd784b70626ca9b
Rutikesh Sawant <68364793+ruti-sawant@users.noreply.github.com>
2022-05-06 15:35:35 +0530

    Change in url for white listing

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 function middleware(req, res, next) {
     console.log("middleware", req.headers.origin);
-    const corsWhitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'https://quirky-heisenberg-5d299a.netlify.app/'];
+    const corsWhitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'https://quirky-heisenberg-5d299a.netlify.app'];
     if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
         console.log("correct origin");
         res.header('Access-Control-Allow-Origin', req.headers.origin);
